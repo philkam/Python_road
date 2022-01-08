bdb1f95e4b291b529435686f5764cf1e9081cd8f
Philip Marfo <philcho98@gmail.com>
2022-01-08 22:53:55 +0000

    Elaboration of comments in python

---
+++ 2-Comments.py
@@ -0,0 +1,15 @@
+#Comments are for documentation of code to help people understand your code. This is a comment.
+
+# The # is for put before a single line.
+
+# The """  is put before and after the lines in case your comment is multiple line.
+
+#Below is a multiline comment
+"""Hello this is a python road which gives a description of my python learning process.
+I want to visit this code when I am preparing for python interviews to refresh my pyhton knowledge."""
+
+a = 1
+b = 2
+c = a + b #this adds two numbers(1  & 2) together. This line will not be read my interpreter.
+
+print(c) #this shows the output of c.
