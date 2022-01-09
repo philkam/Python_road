6d19321353eb7be076d7931ba75cc7b5868285b2
Philip Marfo <philcho98@gmail.com>
2022-01-09 22:49:55 +0000

    Python Variables and their conventions

---
+++ 5-PythonVariables.py
@@ -0,0 +1,19 @@
+#Naming variables is a good practice. Name variables with meaningful names.
+
+#Python reserved keywords  can't be used to name variables. Example: if, for, while etc.
+
+#Python is case-sensitive. X is not the same as x.
+
+#Variable names should be lowercase, with words separated by underscores as neccessary to improve readability.
+#example: age , student_id.
+
+
+print (5 + 9)
+#You can't get access to the answer after you have computed it. it will be lost in memory because no variable is used but
+# x belows stores the value of the operation
+
+x = 5 + 8
+print(x)
+
+# x can be reused for other purposes. That's why variables are necessary
+print(x - 4)
