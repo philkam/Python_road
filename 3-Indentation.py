d0f042371dd8231aa0b308423131a4eb452c1796
Philip Marfo <philcho98@gmail.com>
2022-01-09 22:08:20 +0000

    Indentation in Python

---
+++ 3-Indentation.py
@@ -0,0 +1,5 @@
+#Defines blocks of code
+#A way to tell python the flow of your nested blocks
+#Tab or 4 spaces are used to define a block of code. In other languages it is ()
+#All the statements within the same block should have the same indentation level
+#Refer to docs for pyhton indentation.
