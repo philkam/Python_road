43c17de0a5396c24d4adea21ab75ba1da8a7833a
Philip Marfo <philcho98@gmail.com>
2022-01-09 22:38:12 +0000

    Python Data Types Overview

---
+++ 4-BasicDataTypes.py
@@ -0,0 +1,29 @@
+""""Python variables are containers for storing data values. Python doesn't have commands for  processing variables.
+Variables are created immediately a value is assigned to it. Types are below"""
+# Integers and float
+# Strings
+# Booleans
+
+#Numeric types- Integer, float, complex numbers
+#Boolean- Notice that 'T' and 'F' are capital for True and False
+
+#***********************************************************************************************************
+
+
+"""Sequence Type: An ordered collection of similar or different data types. Python has the following built-in types: 
+1. String- put in double or triple quotes
+2. List - put in  Square brackets
+3. Tuple - put in parenthesis
+4. Dictionary - enclosed in curly brackets. it is a key:value pair. Eg {1:"Monica", 2:"Jennifer"}
+"""
+#***********************************************************************************************************
+
+"""NB
+type() function- it ascertain the data type of a certain value. type(123) returns <class 'int'> meaning integer
+
+Mutable and Immutable objects: 
+Immutable means their contents cannot be altered after creation. Number values, strings and tuples are immutable
+Mutable means the collection of items can be modified. Dictionary and list are mutable. Mutable items can be added, deleted, 
+inserted, rearranged etc"""
+
+
