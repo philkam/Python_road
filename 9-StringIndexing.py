f54b4ba2dc21be1ff2b3390bb258ef979a93a3a8
Philip Marfo <philcho98@gmail.com>
2022-01-11 23:57:42 +0000

    Indexing of  Strings in python

---
+++ 9-StringIndexing.py
@@ -0,0 +1,13 @@
+#String Indexing
+#Indexing means you want to access a part of the string
+#It is done using [] and is zero-based.
+#Indexing can be done in reverse order, where -1 corresponds to the last character in the string.
+#Bigger indexes than the length of the string will throw an error.
+
+
+#usage
+
+first_string = "Kenny"
+print(first_string[0]) #fetches the first element
+print(first_string[-1]) #fetches the last element in string
+#print(first_string[9]) #error due to inadequate length
