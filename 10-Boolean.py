45ba6d1b5d7a0475d03e5ad07d710c5a1e523296
Philip Marfo <philcho98@gmail.com>
2022-01-12 23:42:49 +0000

    Boolean Data Type in  Python

---
+++ 10-Boolean.py
@@ -0,0 +1,10 @@
+#Boolean Data Type
+#It can have one of two values; True or False.
+#The T in 'True' should be capital and the F in 'False' should also be capital.
+#It shouldn't be TRUE, FALSE, true, false.
+#Any python expression can be either True or False. All expressions are boolean
+
+
+#Usage
+x = True
+print(type(x))
