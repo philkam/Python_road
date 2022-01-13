3f4f6a80b2e101a0cd1640ae5fbfd0fa221e891e
Philip Marfo <philcho98@gmail.com>
2022-01-13 22:35:08 +0000

    Overview of Python Operators

---
+++ 12-PythonOperators.py
@@ -0,0 +1,12 @@
+#Python Operators
+#operators carry out arithmetic or logical computations. Operations are performed on variables and values
+#They are:
+"""Arithmetic Operators: Used with numerical values to perform mathematical operations. eg. + and -
+    Assignment Operators : Used to assign values to variables  eg. = and +=
+    Comparison Operators  : Used to compare two values  eg. == and !=
+    Logical Operators   : Used to combine conditional statements eg. and as well as or
+    Identity Operators  : Used to compare the objects not if they are equal but if they are the same eg. is and is not
+    Membership Operators  :   Used to test if a sequence is presented in an object eg. in and in not
+    Bitwise Operators:  Used to compare binary numbers eg. AND (&) and OR (|) and XOR (^)
+"""
+
