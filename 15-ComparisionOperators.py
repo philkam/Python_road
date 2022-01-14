2852e6dc3bea9dac3ad9f9f42c735456fe09ad98
Philip Marfo <philcho98@gmail.com>
2022-01-14 21:14:47 +0000

    Comparison Operators in Python

---
+++ 15-ComparisionOperators.py
@@ -0,0 +1,20 @@
+#Comparison Operators
+"""
+Equal to -> ==
+Not Equal to -> !=
+Less than = <
+Less than or equal to = <=
+Greater than = >
+Greater than or equal to - >=
+
+Check the python documentation: Floating point arithmetic:issues and limitations
+"""
+
+#Usage
+a = 4
+b = 3
+
+print(a == b)
+print(a < b)
+print(a <= b)
+print(a > b)
