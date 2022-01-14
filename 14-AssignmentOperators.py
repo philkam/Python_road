ae23cef3f85bba6025060875c0201bae32e4b813
Philip Marfo <philcho98@gmail.com>
2022-01-14 20:58:28 +0000

    Assignment Operators in Python

---
+++ 14-AssignmentOperators.py
@@ -0,0 +1,20 @@
+#Assignment Operators
+"""
+Assign -> =
+Add and assign -> +=
+Subtract and assign -> -=
+Multiply and assign -> *=
+Modulus and assign -> %=
+Exponent and assign -> **=
+Floor and assign -> //=
+
+"""
+#Usage
+
+a = 4
+a = a + 4 #is the same as a+=4
+print("The result of a+=4 is ", a)
+
+b = 4
+b += 4 # is the same as b = b+4
+print("The result of b = b + 4 is ", b)
