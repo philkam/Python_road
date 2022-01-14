2f7962dd81736ed67b3dc39e9f8c65a65ade1ed0
Philip Marfo <philcho98@gmail.com>
2022-01-14 22:14:04 +0000

    Membership Operators in Python

---
+++ 18-MembershipOperators.py
@@ -0,0 +1,7 @@
+#Membership operators
+#in and not in
+#It is used to identify if a sequence is presnt in another or not.
+
+#Usage
+first_string = "Is Marty Byrde in Ozarks?"
+print("marty" in first_string)
