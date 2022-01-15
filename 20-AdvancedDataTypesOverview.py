a5004e90a4b19496adc8557115596131c2ffbf41
Philip Marfo <philcho98@gmail.com>
2022-01-15 22:15:09 +0000

    Advanced Data Types in Python

---
+++ 20-AdvancedDataTypesOverview.py
@@ -0,0 +1,19 @@
+#Python Advanced Data Types
+"""
+List: It is a collection of different data types. It is an ordered sequence of items. A list object contains one or more items,
+not necessarily of the same type, which are separated by comma and enclosed in square brackets[].
+Syntax: list = [value1, value2, value3]
+
+Tuples: Tuple is a collection of items of any python data type, same as the list type. Unlike the list, tuple is immutable. The tuple
+object can include elements separated by comma without parentheses.
+syntax: tuple = ["value1, value2, value3]
+
+Set: It is not an ordered collection of objects. A set object performs mathematical set operations like union,intersection,
+difference etc. A set object contains one or more items.
+Syntax: set = {value1, value2, value3}
+
+Dictionary: It is a collection type. It contains key-value pairs. ONe or more key:value pairs separated by commas are
+put inside curly brackets to form a dictionary objects. The same key cannot appear more than once in a collections.
+If the key appears more than once, only the last will be retained. One value can be assigned to more than one key.
+Syntax: {key1:value1, key2:value2, key3:value3}
+"""
