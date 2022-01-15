f91326bcbff8072d346693c916e479ba0a5ea3e2
Philip Marfo <philcho98@gmail.com>
2022-01-15 22:20:53 +0000

    Overview of List

---
+++ 21-ListOverview.py
@@ -0,0 +1,16 @@
+#List
+#Multiple things/objects can be stored in the same variable.
+#List is a collection of any objects(integer, string).
+#We use square brackets[] separated by commas.
+#List indexing and slicing is very important.
+
+#Usage
+x = 2
+y = 4.2
+z = "Phil"
+f = False
+
+my_list = [x, y, z, f]
+
+print(my_list)
+print(type(my_list))
