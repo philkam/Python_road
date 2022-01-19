410d349662df6c5b771cf77ca9688cec9d8c90ed
Philip Marfo <philcho98@gmail.com>
2022-01-19 23:26:38 +0000

    When to use each of the data types in Python

---
+++ 26-WhenToUseEachDataType.py
@@ -0,0 +1,10 @@
+#When to use each data type in Python
+"""List: USe lists when we do not have any special cases that we want to take care of,
+and we want our list to be ordered for indexing.
+Tuples: Usge tuples only when we are sure that the values inside them should not be changed no matter what,
+so this is the best way to assure that.
+Dictionaries: Use dictionaries when we want to have some sort of relation between some unique
+variables and other non-unique variables,as we only concerned with the key
+Sets: Use sets when we know that any repeated data will be redundant. So, sets can be very efficient in ignoring redundant data and
+increase the performance of any algorithm
+"""
