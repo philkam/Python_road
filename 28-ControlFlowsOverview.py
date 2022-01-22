8a7eaa7381c49685a2279828e1fb63596c088afb
Philip Marfo <philcho98@gmail.com>
2022-01-22 18:11:31 +0000

    Introduction to Control flow

--- 28-ControlFlowsOverview.py
+++ 28-ControlFlowsOverview.py
@@ -2,3 +2,6 @@
 #Control flow is the order in which the programs code is executed.
 #Conditional Statements - if, else, elif
 #For and while loops: Iteration means executing the same git
+
+
+#COntrol flow  is programming power. it contains logic that tells controls the program.
