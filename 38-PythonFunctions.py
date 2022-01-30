0ed49455f98cf80734c0c555650ca80d994317a8
Philip Marfo <philcho98@gmail.com>
2022-01-30 20:20:28 +0000

    Overview of Python Functions

---
+++ 38-PythonFunctions.py
@@ -0,0 +1,8 @@
+#Function
+#A function is a group of related statments that perform a specific task.
+#Function helps to break programs into smaller organised and manageable way
+#We will be looking at
+#- Help function
+#- Definining Functions
+#-Variable scope(Local Vs Global)
+#-Doc Strings
