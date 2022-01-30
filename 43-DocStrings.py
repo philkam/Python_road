80ec4ee3cb57f3f4b7f891325bc13fd8e58d596b
Philip Marfo <philcho98@gmail.com>
2022-01-30 21:16:15 +0000

    Documentation Strings in Python

---
+++ 43-DocStrings.py
@@ -0,0 +1,8 @@
+#Documentation
+"""
+Doc Strings, for documentation strings, is crucial for documenting python functions.
+Very simple, just triple quotation strings in the line after the  function header.
+Explains what a function is doing.
+If we have a complex function, we can add more details to the docstrings.
+Eg. Explain what the function is doing, its inputs and outputs.
+"""
