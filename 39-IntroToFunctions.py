50754696e26e87def6c2b15f1bc83ddb6297c7b4
Philip Marfo <philcho98@gmail.com>
2022-01-30 20:24:48 +0000

    Introduction to Functions in Python

---
+++ 39-IntroToFunctions.py
@@ -0,0 +1,6 @@
+#Functions
+#Functions take some inputs, do some logic on them, and return an output.
+#Python is built around functional programming and OOP.
+#Imagine that you want to calculate the sum of two variables.
+#What if you want to repeat the process the multiple time?
+#You want to have it as a function in your code so as to make the repetition easier.
