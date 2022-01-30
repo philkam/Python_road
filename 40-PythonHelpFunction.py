6602df86113159b6bbeeed2c2c1775d4f8d71a83
Philip Marfo <philcho98@gmail.com>
2022-01-30 20:29:59 +0000

    Usage of Help Function in Python

---
+++ 40-PythonHelpFunction.py
@@ -0,0 +1,9 @@
+#Python Help Function
+#Python has hundred of built-in functions.
+#Each function cqn have one or more inputs and outputs.
+#There is no need to remember how to use any of them.
+#The help function can help you do that.
+#Also, most of the famous libraries in python have their help function.
+
+#usage
+help(print)
