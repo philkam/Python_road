6a71fe909fda91e51964eac373326b0490c78524
Philip Marfo <philcho98@gmail.com>
2022-02-01 19:10:20 +0000

    Introduction to Error Handling in Python

---
+++ 44-UserInputAndErrorsHandling.py
@@ -0,0 +1,12 @@
+#Error
+"""Error Handling -  Python program terminates as soon as there is an error. Error handling will make us understand how to
+go about errors. We can have syntax errors and logical errors(exceptions)"""
+#Almost anything in the world can have errors at some point, especially in programming.
+
+#Error Types
+"""
+Syntax Errors: The code won't run under any condition.
+
+Exception: Can happen if there are no syntax errors. Code will stop if some condition happens.
+            We need to catch and handle this condition, so the code wont stop.
+            """
