26ecddf4c4e8e267bfd83f76636e2a123eff925e
Philip Marfo <philcho98@gmail.com>
2022-02-04 22:39:06 +0000

    Syntax Errors in Python

---
+++ 46-SyntaxError.py
@@ -0,0 +1,24 @@
+#Syntax Error
+"""
+It is also known as parsing errors, as the Python interpreter parse the whole code
+to convert it to byte code before being executed.
+It is most common type for beginners.
+It is unrelated to correctness of the code's logic. it must be fixed for code to run.
+In reading a syntax error, Check
+1. File name
+2. Exponent or caret sign
+3. THe line number
+4. Error message
+"""
+
+#Usage
+first_num = 20
+1secondnum = 30 #Variable naming cannot start with one. Syntax error
+third_num = 50
+forth num = 32 #variable naming cannot have spacing. Syntax error
+
+#Syntax error solutions
+#remove 1 from the variable like "second_num"
+#remove the space form the variable like "forth_num"
+#After this the code will run perfectly fine
+
