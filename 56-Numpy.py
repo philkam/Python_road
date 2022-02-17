ff40bd64ca17d95afcfc1b1da9d4cc54f5c8cc9a
Philip Marfo <philcho98@gmail.com>
2022-02-17 23:56:50 +0000

    Introduction to Numpy

---
+++ 56-Numpy.py
@@ -0,0 +1,17 @@
+#Overview
+#Numpy is a free python library that is equipped with complex mathematical operations that are suitable for processing
+#statistical data.
+#All data is stored in NumPy Arrays.
+
+#Intro
+"""
+-It means numerical python. it is a must for scientific computing.
+-The main component is the NumPy N-dimensional Array (ndarray)
+"""
+
+#Usages of Numpy
+"""
+- Faster than python arrays
+-N-dimensional arrays by nature. Optimised and easier to work with more complex data
+-Dozens of built-in functions.
+Uses: scientific domain, dozens of third-party libraries, data science, machine learning, visualisation"""
