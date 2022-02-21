3ef4ea56641cc83cdf657e0ce2fe7a8aeb6c7478
Philip Marfo <philcho98@gmail.com>
2022-02-21 19:27:34 +0000

    Logical Array Indexing of Numpy Arrays in python

---
+++ 61-LogicalArrayIndexingNumPy.py
@@ -0,0 +1,13 @@
+#Array Logical Indexing
+#Sometimes,you want to select some elements from an array based on a condition.
+
+
+#Usage
+import numpy as np
+
+np_arr = np.array([[1,5,3], [4,1,6], [2,4,10]])
+
+np_cond = np_arr[np_arr>3]
+print(np_cond)
+print(np_arr>3)
+
