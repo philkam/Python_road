a32699f83bcfb754e466a7e7bc681543863f8924
Philip Marfo <philcho98@gmail.com>
2022-02-22 22:59:32 +0000

    Introduction to Pandas in python

---
+++ 63-Pandas.py
@@ -0,0 +1,12 @@
+#Pandas
+"""
+-Name comes from panel data, which is an econometrics term.
+-Crucial for data manipulation and analysis
+-Introduces two new data structures, Pandas DataFrames and Pandas Series.
+-DataFrame is perfect for tabular data manipulation
+-Easy-to-use functions for interfacing with text files, excel sheets, SQL databases, and HDF5 binary files.
+-Flexible in labeling rows and columns
+-Highly integrable with NumPy, MatPlotLib, and hundreds of powerful libraries.
+-Dozens of data cleaning, manipulation, and analysis functions.
+-Check documentation for more details
+"""
