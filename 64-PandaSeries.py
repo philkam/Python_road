aca9144e5a382101b6841fe997e2f85bcd7c18a5
Philip Marfo <philcho98@gmail.com>
2022-02-25 00:07:16 +0000

    Panda Series in python

--- 64-PandaSeries.py
+++ 64-PandaSeries.py
@@ -19,13 +19,42 @@ print(series_np) #the index and values are printed.
 series_list = pd.Series([1, 10, 3])
 print(series_list)
 
-##from dictionary
+## from dictionary
 di = {"First": 3, "Second": 2.1, "Apple": "Orange"}
-series_dict = pd.Series(di)
+series_dict = pd.Series(di, name = "Dict")
 print(series_dict)
 #print(series_dict['First'])
 
-##from scalar value
+## from scalar value
 scalar_value = 10
 series_scalar = pd.Series(scalar_value, index=[0,1,2,3,4])
 print(series_scalar)
+
+
+#numpy-like
+print(series_dict.dtype)
+np_arr = series_scalar.to_numpy()
+print(np_arr)
+
+#dict-like
+print(series_scalar.get(4))
+
+#Attributes
+
+## shape
+print(series_dict.shape)
+
+## ndim
+print(series_dict.ndim)
+
+## size
+print(series_dict.size)
+
+#values
+print(series_dict.values)
+
+#index
+print(series_dict.index)
+
+#name
+print(series_dict.name)
