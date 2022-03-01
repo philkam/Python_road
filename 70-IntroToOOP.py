4a2bd26eeacc9703fa4ccd500ab3d10ddff849fd
Philip Marfo <philcho98@gmail.com>
2022-03-01 20:53:11 +0000

    Intro To OOP in Java

---
+++ 70-IntroToOOP.py
@@ -0,0 +1,22 @@
+#Difference between Functional & OOP
+"""
+The function is the main unit of the program whilst OOP the class is the main unit of the program
+Functional follows top-down approach whilst OOP follows bottom-up approach
+Functional is not super realistic whilst OOP is much more realistic
+Functional does not care about data whilst OOP data is very important
+Functional is not good wit complex and big projects whilst OOP is very good with complex and big projects
+Functional, there is no way to hide data but OOP, data protection can be achieved.
+"""
+
+#OOP Definitions
+"""
+Classes and Objects
+Classes: The blueprint of the objects. A template. A backbone. For example, a dog class
+Objects: The actual entity we interact with. The instance of the class. For example, 2 years, black
+
+Attribute & Method
+Attribute: the object characteristic. For example, color, length, age etc
+
+Method: the actions(functions) that the object can do. For example, flying, eating, running etc.
+
+"""
