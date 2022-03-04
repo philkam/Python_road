b0f77a6b6dfc584d489e4b137de2e038d3e9d51a
Philip Marfo <philcho98@gmail.com>
2022-03-04 20:59:55 +0000

    Python Decorators

---
+++ 74-PythonDecorators.py
@@ -0,0 +1,25 @@
+#Decorators
+"""
+-It's a function that takes another function as an input
+-So, it's considered as a higher-order function, like the map and filter functions we discussed
+-It extends the behaviour of the input function, without modifying it.
+"""
+
+#Inner Functions
+"""
+-We can define functions inside other functions
+-We call these functions inner functions
+-We can't call the inner functions because they are defined inside the outer functions
+-The code will throw an error
+-We can return an inner function inside the outer function
+-We will return in fact the reference of this function so we will not have parenthesis
+"""
+
+#Decorators: They wrap a function and modify its functionality. It's almost the same as inner functions
+
+#Pie Syntax
+"""
+-@ symbol is used to wrap function to prevent it from being unnecessarily messy"""
+
+#Reuse Decorators
+"""We can reuse the decorators you created to work with any function"""
